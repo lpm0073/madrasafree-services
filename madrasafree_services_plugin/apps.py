146e7aecf105bc56083c608be215dbfca68b2c07
lpm0073 <lpm0073@gmail.com>
2025-03-03 17:43:16 -0600

    fix: misspelled name

--- madrasafree_services_plugin/apps.py
+++ madrasafree_services_plugin/apps.py
@@ -4,26 +4,31 @@ madrasafree_services Django application initialization.
 # Check this for an example of using edx django plugin
 # https://github.com/openedx/edx-bulk-grades/blob/master/bulk_grades/apps.py
 from django.apps import AppConfig
+from django.conf import settings
+from edx_django_utils.plugins import PluginSettings, PluginURLs
+from openedx.core.djangoapps.plugins.constants import ProjectType, SettingsType
 
 
 class MadrasafreeServicesConfigPlugin(AppConfig):
-#Configuration for the madrasafree_services Django application.
-
+    # Configuration for the madrasafree_services Django application.
 
     name = 'madrasafree_services_plugin'
     label = 'mad'
+
+    # See: https://edx.readthedocs.io/projects/edx-django-utils/en/latest/edx_django_utils.plugins.html
     plugin_app = {
-        'url_config': {
-             'lms.djangoapp': {
-                 'namespace': 'madrasa_service_plugin',
-                 'regex': '^madrasa_api/',
-                 'relative_path': 'urls',
-             },
+        PluginURLs.CONFIG: {
+            ProjectType.LMS: {
+                PluginURLs.NAMESPACE: name,
+                PluginURLs.REGEX: "^madrasa_api",
+                PluginURLs.RELATIVE_PATH: "urls",
+            }
         },
-        'settings_config': {
-            'lms.djangoapp': {
-                'production': { 'relative_path': 'settings.production' },
-                'common': { 'relative_path': 'settings.common' },
+        PluginSettings.CONFIG: {
+            ProjectType.LMS: {
+                # uncomment these to activate
+                SettingsType.PRODUCTION: {PluginSettings.RELATIVE_PATH: "settings.production"},
+                SettingsType.COMMON: {PluginSettings.RELATIVE_PATH: "settings.common"},
             }
         },
 
